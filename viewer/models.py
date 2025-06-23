56fdb3ef9d23f89d482eaa7fd12c9c6fb581cfff
franců <francupatrik@seznam.cz>
2025-06-23 22:57:15 +0200

    03 - Úprava ve struktuře databáze, naplnění databáze test databáze

--- viewer/models.py
+++ viewer/models.py
@@ -10,6 +10,7 @@ class Country(models.Model):
 
     class Meta:
         ordering = ['name']
+        verbose_name_plural = 'Countries'
 
     def __str__(self):
             return self.name
@@ -21,10 +22,11 @@ class Country(models.Model):
 class City(models.Model):
     name = models.CharField(max_length=100, unique=False, null=False, blank=False)
     country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name="cities")
-    zip_code = models.CharField(max_length=50, unique=True, null=False, blank=False)
+    zip_code = models.CharField(max_length=50, null=False, blank=False)
 
     class Meta:
         ordering = ['name']
+        verbose_name_plural = 'Cities'
 
     def __str__(self):
         return self.name
